e4b1371e6a9b85cf4da8b13a8f324b84b3667fb1
Andressa Atelli <andressaatelli.programador@gmail.com>
2024-01-11 10:46:55 -0300

    Inclusão da Interface e configuration

---
+++ Dominio/Interfaces/Generics/InterfaceGeneric.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Interfaces.Generics
+{
+    public interface InterfaceGeneric<T> where T : class
+    {
+        Task Add(T objeto);    
+
+        Task Update(T objeto);
+
+        Task Delete(T objeto);
+
+        Task<T> GetEntityById(int Id);
+
+        Task<List<T>> List();
+    }
+}
---
+++ InfraEstrutura/Configuracao/ContextBase.cs
@@ -0,0 +1,46 @@
+﻿using Entities.Entidades;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace InfraEstrutura.Configuracao
+{
+    public class ContextBase : IdentityDbContext<ApplicationUser>
+    {
+        public ContextBase(DbContextOptions options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<SistemaFinanceiro> SistemaFinanceiro { get; set; }
+
+        public DbSet<UsuarioSistemaFinanceiro> UsuarioSistemaFinanceiro { get; set; }
+
+        public DbSet<Categoria> Categoria {  get; set; }
+
+        public DbSet<Despesa> Despesas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(ObterStringConexao());
+                base.OnConfiguring(optionsBuilder);
+            }
+
+        }
+
+        //Mapear a tabela
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUser>().ToTable("AspNetUsers").HasKey(t => t.Id);
+
+            base.OnModelCreating(builder);
+        }
+        
+        public string ObterStringConexao()
+        {
+
+            return "Data Source=DESKTOP-30B570N\\SQLEXPRESS;Initial Catalog=FINANCEIRO_2023;Integrated Security=True";
+        }
+    }
+}
