5edb7b58d3a1a23b5fa4b8d2cc9f7fa41634857d
Andressa Atelli <andressaatelli.programador@gmail.com>
2024-01-11 15:42:53 -0300

    Instalando dependencias na webapi

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,3 +1,11 @@
+using Dominio.Interfaces.Generics;
+using Entities.Entidades;
+using InfraEstrutura.Configuracao;
+using InfraEstrutura.Repositorio.Generics;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +14,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ContextBase>(options =>
+                options.UseSqlServer(
+                    builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ContextBase>();
+
+
+//Interface Repositorio
+builder.Services.AddSingleton(typeof(InterfaceGeneric<>), typeof(RepositoryGenerics<>));
+
 
 var app = builder.Build();
 
