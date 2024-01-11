0ca390daca9b22a2e41238d99f5ab3e58d2c439f
Andressa Atelli <andressaatelli.programador@gmail.com>
2024-01-10 21:16:37 -0300

    Inserindo Entities

---
+++ Entities/Entidades/Base.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities.Entidades
+{
+    public class Base
+    {
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+
+        [Display(Name = "Nome")]
+        public string Nome { get; set; }    
+    }
+}
