aa51ce5ac55b2ce31ea13ab6d3ba79337859fdb3
Andressa Atelli <andressaatelli.programador@gmail.com>
2024-01-15 20:56:08 -0300

    IUsuarioSistemaFinanceiro

--- Dominio/Interfaces/IServico/IUsuarioSistemaFinanceiroServico.cs
+++ Dominio/Interfaces/IServico/IUsuarioSistemaFinanceiroServico.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace Dominio.Interfaces.IServico
 {
     public interface IUsuarioSistemaFinanceiroServico
     {
+        Task CadastrarUsuarioSistema(UsuarioSistemaFinanceiro usuarioSistemaFinanceiro);
     }
 }
