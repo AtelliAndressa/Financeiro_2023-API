04c345bb5b38dc9af3e943335b88c9bfd3eddc2a
Andressa Atelli <andressaatelli.programador@gmail.com>
2024-01-15 15:30:41 -0300

    Inclusao do RepositorioCategoria

--- InfraEstrutura/Repositorio/RepositorioCategoria.cs
+++ InfraEstrutura/Repositorio/RepositorioCategoria.cs
@@ -1,14 +1,31 @@
 ﻿using Dominio.Interfaces.ICategoria;
 using Entities.Entidades;
+using InfraEstrutura.Configuracao;
 using InfraEstrutura.Repositorio.Generics;
+using Microsoft.EntityFrameworkCore;
 
 namespace InfraEstrutura.Repositorio
 {
     public class RepositorioCategoria : RepositoryGenerics<Categoria>, InterfaceCategoria
     {
-        public Task<IList<Categoria>> ListarCategoriasUsuario(string emailUsuario)
+        private readonly DbContextOptions<ContextBase> _optionsBuilder;
+
+        public RepositorioCategoria()
         {
-            throw new NotImplementedException();
+            _optionsBuilder = new DbContextOptions<ContextBase>();
+        }
+
+        public async Task<IList<Categoria>> ListarCategoriasUsuario(string emailUsuario)
+        {
+            using (var banco= new ContextBase(_optionsBuilder))
+            {
+                return await
+                    (from s in banco.SistemaFinanceiro
+                     join c in banco.Categoria on s.Id equals c.Id
+                     join us in banco.UsuarioSistemaFinanceiro on s.Id equals us.IdSistema
+                     where us.EmailUsuario.Equals(emailUsuario) && us.SistemaAtual
+                     select c).AsNoTracking().ToListAsync();
+            }
         }
     }
 }
