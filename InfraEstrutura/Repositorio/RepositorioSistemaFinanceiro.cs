9dbdbf2fd88cdcbe013177858887bf65f7319458
Andressa Atelli <andressaatelli.programador@gmail.com>
2024-01-15 20:33:46 -0300

    Inserção do repositorio do Sistema Financeiro

--- InfraEstrutura/Repositorio/RepositorioSistemaFinanceiro.cs
+++ InfraEstrutura/Repositorio/RepositorioSistemaFinanceiro.cs
@@ -1,14 +1,30 @@
 ﻿using Dominio.Interfaces.ISistemaFinanceiro;
 using Entities.Entidades;
+using InfraEstrutura.Configuracao;
 using InfraEstrutura.Repositorio.Generics;
+using Microsoft.EntityFrameworkCore;
 
 namespace InfraEstrutura.Repositorio
 {
     public class RepositorioSistemaFinanceiro : RepositoryGenerics<SistemaFinanceiro>, InterfaceSistemaFinanceiro
     {
-        public Task<IList<SistemaFinanceiro>> ListarSistemasUsuario(string emailUsuario)
+        private readonly DbContextOptions<ContextBase> _optionsBuilder;
+
+        public RepositorioSistemaFinanceiro()
         {
-            throw new NotImplementedException();
+            _optionsBuilder = new DbContextOptions<ContextBase>();
+        }
+
+        public async Task<IList<SistemaFinanceiro>> ListarSistemasUsuario(string emailUsuario)
+        {
+            using (var banco = new ContextBase(_optionsBuilder))
+            { 
+                return await
+                    (from s in banco.SistemaFinanceiro
+                     join us in banco.UsuarioSistemaFinanceiro on s.Id equals us.IdSistema
+                     where us.EmailUsuario.Equals(emailUsuario)
+                     select s).AsNoTracking().ToListAsync();
+            }
         }
     }
 }
