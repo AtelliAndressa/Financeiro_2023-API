f3b03a310c79a1e9cee7ec2689262b89d944163d
Andressa Atelli <andressaatelli.programador@gmail.com>
2024-01-15 20:53:39 -0300

    IDespesaServico

--- Dominio/Interfaces/IServico/ICategoriaServico.cs
+++ Dominio/Interfaces/IServico/ICategoriaServico.cs
@@ -1,9 +1,4 @@
 ﻿using Entities.Entidades;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Dominio.Interfaces.IServico
 {
