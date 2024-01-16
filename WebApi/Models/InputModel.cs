58404e6de05e3135e6f73f8d81cdcbed368e9aa1
Andressa Atelli <andressaatelli.programador@gmail.com>
2024-01-16 16:35:52 -0300

    criação do controller para criar o token

---
+++ WebApi/Models/InputModel.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApi.Models
+{
+    /// <summary>
+    /// email e password do usuario para geração de token
+    /// </summary>
+    public class InputModel
+    {
+        public string Email { get; set; }   
+
+        public string Password { get; set; }
+    }
+}
