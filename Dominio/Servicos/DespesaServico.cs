1e6936ccc73e0ed2dc2edebbf0fa3b61895dd556
Andressa Atelli <andressaatelli.programador@gmail.com>
2024-01-16 14:28:01 -0300

    servico sistemafinanceiro SistemaFinanceiro : ISistemaFinanceiroServico

---
+++ Dominio/Servicos/DespesaServico.cs
@@ -0,0 +1,56 @@
+﻿using Dominio.Interfaces.IDespesa;
+using Dominio.Interfaces.IServico;
+using Entities.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Servicos
+{
+    public class DespesaServico : IDespesaServico
+    {
+        private readonly InterfaceDespesa _interfaceDespesa;
+
+        public DespesaServico(InterfaceDespesa interfaceDespesa)
+        {
+            _interfaceDespesa = interfaceDespesa;
+        }
+
+        public async Task AdicionarDespesa(Despesa despesa)
+        {
+            var data = DateTime.UtcNow;
+            despesa.DataCadastro = data;
+            despesa.Ano = data.Year;
+            despesa.Mes = data.Month;
+
+            var valido = despesa.ValidarPropriedadeString(despesa.Nome, "Nome");
+
+            if (valido)
+            {
+                await _interfaceDespesa.Add(despesa);
+            }
+        }
+
+        public async Task AtualizarDespesa(Despesa despesa)
+        {
+            var data = DateTime.UtcNow;
+            despesa.DataAlteracao = data;
+
+            if (despesa.Pago)
+            {
+                despesa.DataPagamento = data;
+            }
+
+            var valido = despesa.ValidarPropriedadeString(despesa.Nome, "Nome");
+
+            if (valido)
+            {
+                await _interfaceDespesa.Update(despesa);
+            }
+        }
+    }
+}
+}
---
+++ Dominio/Servicos/SistemaFinanceiro.cs
@@ -0,0 +1,44 @@
+﻿using Dominio.Interfaces.IServico;
+using Dominio.Interfaces.ISistemaFinanceiro;
+
+namespace Dominio.Servicos
+{
+    public class SistemaFinanceiro : ISistemaFinanceiroServico
+    {
+        private readonly InterfaceSistemaFinanceiro _interfaceSistemaFinanceiro;
+
+        public SistemaFinanceiro(InterfaceSistemaFinanceiro interfaceSistemaFinanceiro)
+        {
+            _interfaceSistemaFinanceiro = interfaceSistemaFinanceiro;
+        }
+
+        public async Task AdicionarSistemaFinanceiro(Entities.Entidades.SistemaFinanceiro sistemaFinanceiro)
+        {
+            var valido = sistemaFinanceiro.ValidarPropriedadeString(sistemaFinanceiro.Nome, "Nome");
+
+            if (valido)
+            {
+                var data = DateTime.Now;
+
+                sistemaFinanceiro.DiaFechamento = 1;
+                sistemaFinanceiro.Ano = data.Year;
+                sistemaFinanceiro.Mes = data.Month;
+                sistemaFinanceiro.AnoCopia = data.Year;
+                sistemaFinanceiro.MesCopia = data.Month;
+                sistemaFinanceiro.GerarCopiaDespesa = true;
+
+                await _interfaceSistemaFinanceiro.Add(sistemaFinanceiro);
+            }
+        }
+
+        public async Task AtualizarSistemaFinanceiro(Entities.Entidades.SistemaFinanceiro sistemaFinanceiro)
+        {
+            var valido = sistemaFinanceiro.ValidarPropriedadeString(sistemaFinanceiro.Nome, "Nome");
+
+            if (valido)
+            {
+                sistemaFinanceiro.DiaFechamento = 1;
+                await _interfaceSistemaFinanceiro.Update(sistemaFinanceiro);
+            }
+    }
+}
